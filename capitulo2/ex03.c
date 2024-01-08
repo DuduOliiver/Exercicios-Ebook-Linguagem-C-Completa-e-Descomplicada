62205b483c4b83046c0ebee2c2baac67dd99a502
DuduOliiver <eduardoliveira.11@hotmail.com>
2024-01-08 11:30:24 -0300

    exercicio 03

---
+++ capitulo2/ex03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+3) Escreva um programa que leia um número inteiro e depois imprima a mensagem “Valor lido:”, seguido do valor inteiro. Use apenas um comando printf().
+*/
+
+#include <stdio.h>
+
+int main(){
+    int n1;
+    scanf("%d",&n1);
+    printf("Valor lido: %d", n1);
+    return 0;
+}

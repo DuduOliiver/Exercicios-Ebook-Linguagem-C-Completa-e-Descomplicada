28cee3ea8f8846319c48e98a86c8dd0bdf16ec39
DuduOliiver <eduardoliveira.11@hotmail.com>
2024-01-08 11:25:52 -0300

    exercicio 02

---
+++ capitulo2/ex02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+2) Escreva um programa que leia um número inteiro e depois o imprima.
+*/
+
+int main(){
+    int n1;
+    printf("Digite um numero: ");
+    scanf("%d",&n1);
+    printf("Numero: %d", n1);
+    return 0;
+}

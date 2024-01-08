277d4469dbc8edd37e8e9ec666f9cd3a6ddb4e75
DuduOliiver <eduardoliveira.11@hotmail.com>
2024-01-08 11:17:56 -0300

    exercício 01

--- capitulo2/ex01.c
+++ capitulo2/ex01.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+/*1) Elabore um programa que escreva as mensagens “Início do programa” e “Fim” na tela,
+uma em cada linha, usando apenas um comando printf().
+*/
+
 int main(){
-    int x; //declara a variável mas não define o valor
-    printf("x = %d\n",x);
-    x = 5; //define o valor de x como sendo 5
-    printf("x = %d\n",x);
+    printf("Inicio do programa. \nFim \n");
     system("pause");
-    return 0;
+    return(0);
 }
 
-/*
-Saída:
-x = qualquer valor
-x = 5
-
-Motivo dá primeira saída ser um valor aleatório é que uma variável é uma posição de memória em que podemos guardar dados porém não existe memória "vazia", pois a posição de memória está preenchido com números binários, o que muda é estar em uso ou não, quando não utilizada já é preenchido por padrão com "lixo de memória".
-
-
-*/

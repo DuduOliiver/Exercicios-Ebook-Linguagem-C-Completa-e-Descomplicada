949767238437d6435e598a2802788c349b8cd561
DuduOliiver <eduardoliveira.11@hotmail.com>
2024-01-09 04:37:43 -0300

    exercicio 11

---
+++ capitulo2/ex11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*11) Elabore um programa que contenha uma constante qualquer do tipo float. Use o comando
+#define. Imprima essa constante.*/
+
+#define num 2.50
+
+int main(){
+    printf("%f", num);
+    return 0;
+}

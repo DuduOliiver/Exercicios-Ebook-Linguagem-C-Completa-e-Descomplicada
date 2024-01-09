6f7720f322a4f01291af77cffda6d90b4bf09873
DuduOliiver <eduardoliveira.11@hotmail.com>
2024-01-09 03:45:01 -0300

    exercicio 08

---
+++ capitulo2/ex08.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int n1,n2;
+    printf("Digite o valor 1: ");
+    scanf("%d",&n1);
+    printf("Digite o valor 2: ");
+    scanf("%d",&n2);
+    printf("Ordem reversa: %d,%d",n2,n1);
+    return 0;
+}

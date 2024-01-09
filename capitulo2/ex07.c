3d3b5eb1821efad83245e5808744bcfe65f04261
DuduOliiver <eduardoliveira.11@hotmail.com>
2024-01-09 03:39:51 -0300

    exercicio 07

---
+++ capitulo2/ex07.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*7) Elabore um programa que leia um caractere e depois o imprima como um valor inteiro.*/
+
+int main(){
+    char ch1;
+    printf("Digite um caracter: ");
+    scanf("%c",&ch1);
+    printf("Digitado: %d",ch1);
+    //printf("Digitado: %c",ch1); jeito certo
+}
+
+/*
+ao utilizar %d para imprimir um caractere, você está exibindo o valor numérico associado a esse caractere de acordo com a tabela ASCII.
+*/

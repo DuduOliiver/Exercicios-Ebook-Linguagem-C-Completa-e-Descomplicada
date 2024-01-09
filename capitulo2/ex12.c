18d62827efbc92a505ad56b00defc4aa4b2d0bb6
DuduOliiver <eduardoliveira.11@hotmail.com>
2024-01-09 04:42:31 -0300

    exercicio 12

---
+++ capitulo2/ex12.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// 12) Elabore um programa que contenha uma constante qualquer do tipo int. Use o comando const. Imprima essa constante.
+
+int main(){
+    const n1=250;
+    printf("%d", n1);
+    return 0;
+}
